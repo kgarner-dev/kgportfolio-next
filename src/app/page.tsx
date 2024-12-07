016d21e56e059a5df52ba78ca4db53946171fe9f
kaleb.garner <kalebgarner@Macmini.attlocal.net>
2024-12-06 21:58:42 -0600

    hero setup and animated background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Hero } from "@/components/hero";
+
 export default function Home() {
   return (
     <>
+      <Hero headline="Kaleb Garner" subheadline="Web Developer" text="Web Developer based in North Texas. I have been working in the tech industry since 2022 and am currently pursuing my degree in Cybersecurity. My experience spans web design, software development, and interaction design."/>
     </>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,23 @@
+import "./styles/hero.scss"
+import Link from "next/link"
+
+interface Content { headline: string, subheadline: string, text: string }
+
+export function Hero( props: Content ) {
+    return (
+        <section className="hero">
+            <div className="hero__headline">
+                <h1>{ props.headline }</h1>
+                <h2>{ props.subheadline }</h2>
+            </div>
+            <div className="hero__text">
+                <p>{ props.text }</p>
+                
+                <div className="hero__btns">
+                    <Link className="btn" href="/about">About Me</Link>
+                    <Link className="btn" href="/resume">Resume</Link>
+                </div>
+            </div>
+        </section>
+    )
+}
